270cdf118d4c7011df3eefc952b321f36db2d73f
Yvan2Ero <yvankanaj@gmail.com>
2021-12-11 13:48:00 +0100

    Configuration des dependences gradle, firebase

--- android/app/src/main/java/com/mhagroapp/MainApplication.java
+++ android/app/src/main/java/com/mhagroapp/MainApplication.java
@@ -11,6 +11,13 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.BV.LinearGradient.LinearGradientPackage;
+
+import com.facebook.react.bridge.JSIModulePackage; // <- add
+import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
+
+
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -26,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new LinearGradientPackage());
           return packages;
         }
 
@@ -33,6 +41,12 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+
+         // Pour reanimated 2
+        @Override protected JSIModulePackage getJSIModulePackage () {
+          return new ReanimatedJSIModulePackage (); // <- add
+        }
+
       };
 
   @Override
