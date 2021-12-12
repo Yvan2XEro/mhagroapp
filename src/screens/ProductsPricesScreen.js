792c0bbee6195dc71c489e264540861d85efd950
Yvan2Ero <yvankanaj@gmail.com>
2021-12-12 09:26:13 +0100

    Retouche du design de l'ecran de listing de prix

--- src/screens/ProductsPricesScreen.js
+++ src/screens/ProductsPricesScreen.js
@@ -1,8 +1,10 @@
 import React, {useCallback, useRef, useState} from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import PricesSheet from '../components/PricesSheet';
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 import ProductPricesSlider from '../components/ProductPricesSlider';
+import {Avatar} from 'react-native-paper';
+import {theme} from '../../styles';
 
 const ProductsPricesScreen = () => {
   const panelRef = useRef(null);
@@ -20,14 +22,79 @@ const ProductsPricesScreen = () => {
   }, []);
   return (
     <BottomSheetModalProvider>
-      <ScrollView>
-        <ProductPricesSlider onPressInItem={() => openInSheet()} />
-        <ProductPricesSlider onPressInItem={() => openInSheet()} />
-        <ProductPricesSlider onPressInItem={() => openInSheet()} />
-        <ProductPricesSlider onPressInItem={() => openInSheet()} />
-        <ProductPricesSlider onPressInItem={() => openInSheet()} />
-        <ProductPricesSlider onPressInItem={() => openInSheet()} />
-        <ProductPricesSlider onPressInItem={() => openInSheet()} />
+      <ScrollView style={{paddingTop: 10}}>
+        <View style={{marginBottom: 10}}>
+          <View style={[styles.row, styles.header]}>
+            <Avatar.Image
+              source={{
+                uri: 'https://cdn.pixabay.com/photo/2018/05/19/18/05/pineapple-3413953__340.jpg',
+              }}
+              size={45}
+            />
+            <Text style={{color: 'white', fontSize: 16}}>Un super produit</Text>
+          </View>
+          <ProductPricesSlider onPressInItem={() => openInSheet()} />
+        </View>
+        <View style={{marginBottom: 10}}>
+          <View style={[styles.row, styles.header]}>
+            <Avatar.Image
+              source={{
+                uri: 'https://cdn.pixabay.com/photo/2018/05/19/18/05/pineapple-3413953__340.jpg',
+              }}
+              size={45}
+            />
+            <Text style={{color: 'white', fontSize: 16}}>Un super produit</Text>
+          </View>
+          <ProductPricesSlider onPressInItem={() => openInSheet()} />
+        </View>
+        <View style={{marginBottom: 10}}>
+          <View style={[styles.row, styles.header]}>
+            <Avatar.Image
+              source={{
+                uri: 'https://cdn.pixabay.com/photo/2018/05/19/18/05/pineapple-3413953__340.jpg',
+              }}
+              size={45}
+            />
+            <Text style={{color: 'white', fontSize: 16}}>Un super produit</Text>
+          </View>
+          <ProductPricesSlider onPressInItem={() => openInSheet()} />
+        </View>
+        <View style={{marginBottom: 10}}>
+          <View style={[styles.row, styles.header]}>
+            <Avatar.Image
+              source={{
+                uri: 'https://cdn.pixabay.com/photo/2018/05/19/18/05/pineapple-3413953__340.jpg',
+              }}
+              size={45}
+            />
+            <Text style={{color: 'white', fontSize: 16}}>Un super produit</Text>
+          </View>
+          <ProductPricesSlider onPressInItem={() => openInSheet()} />
+        </View>
+        <View style={{marginBottom: 10}}>
+          <View style={[styles.row, styles.header]}>
+            <Avatar.Image
+              source={{
+                uri: 'https://cdn.pixabay.com/photo/2018/05/19/18/05/pineapple-3413953__340.jpg',
+              }}
+              size={45}
+            />
+            <Text style={{color: 'white', fontSize: 16}}>Un super produit</Text>
+          </View>
+          <ProductPricesSlider onPressInItem={() => openInSheet()} />
+        </View>
+        <View style={{marginBottom: 10}}>
+          <View style={[styles.row, styles.header]}>
+            <Avatar.Image
+              source={{
+                uri: 'https://cdn.pixabay.com/photo/2018/05/19/18/05/pineapple-3413953__340.jpg',
+              }}
+              size={45}
+            />
+            <Text style={{color: 'white', fontSize: 16}}>Un super produit</Text>
+          </View>
+          <ProductPricesSlider onPressInItem={() => openInSheet()} />
+        </View>
       </ScrollView>
       <PricesSheet
         handlePresentModalPress={handlePresentModalPress}
@@ -41,5 +108,20 @@ const ProductsPricesScreen = () => {
 };
 
 export default ProductsPricesScreen;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: theme.colors.primary,
+  },
+  row: {
+    flexDirection: 'row',
+    backgroundColor: 'red',
+    paddingHorizontal: 7,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  labelText: {
+    fontSize: 17,
+    flex: 3,
+    fontWeight: 'bold',
+  },
+});
